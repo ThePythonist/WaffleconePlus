aa62d53e0fede8c219fd6d5356d7c662c371cef0
ThePythonist <gbt505@hotmail.com>
2020-04-16 01:01:50 +0100

    Disabling http

--- public/js/fiddle.js
+++ public/js/fiddle.js
@@ -20,7 +20,7 @@ const scrubTimeFontSize = 25;
 
 let fullscreen = false;
 
-if (location.protocol !== 'https:') {
+if (location.hostname !== "localhost" && location.hostname !== "127.0.0.1" && location.protocol !== "https:") {
     location.replace(`https:${location.href.substring(location.protocol.length)}`);
 }
 
